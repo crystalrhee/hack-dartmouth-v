1d1dcefcdf644bcc0c7776fedcfbd36ea73e1f4e
KennethSee <kennethseede@gmail.com>
2019-04-06 22:41:08 -0400

    add d3 code

--- client/src/chart.js
+++ client/src/chart.js
@@ -8,19 +8,37 @@ class BarChart extends Component {
 
     drawChart() {
   
-        const data = [12, 5, 6, 6, 9, 10];
-          
-        const svg = d3.select("body").append("svg").attr("width", 700).attr("height", 300);
+        var date1 = new Date(2019,4,1);
+        var date2 = new Date(2019,4,2);
+        var date3 = new Date(2019,4,3);
+        var date4 = new Date(2019,4,4);
+        var date5 = new Date(2019,4,5);
+        var date6 = new Date(2019,4,6);
+        const data = {
+            date1 : {"gym": 0.4, "lunch": 0.2, "work": 0.4},
+            date2 : {"gym": 0.3, "lunch": 0.1, "work": 0.6},
+            date3 : {"gym": 0.2, "lunch": 0.2, "work": 0.6},
+            date4 : {"gym": 0.3, "lunch": 0.8, "work": 0.1},
+            date5 : {"gym": 0.3, "lunch": 0.5, "work": 0.2},
+            date6 : {"gym": 0.3, "lunch": 0.2, "work": 0.5},
+        };
+        const dates = Object.keys(data)
+         
+        const margin = 60;
+        const width = 700
+        const height = 300
+        const svg = d3.select('svg');
+        const chart = svg.append('g')
+            .attr('transform', `translate(${margin}, ${margin})`);
 
-        svg.selectAll("rect")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * 70)
-        .attr("y", 0)
-        .attr("width", 25)
-        .attr("height", (d, i) => d)
-        .attr("fill", "green");
+        var xScale = d3.scaleTime()
+          .domain(dates)
+          .nice(d3.timeDay)
+          .range([0, width]);
+        
+          const yScale = d3.scaleLinear()
+          .range([height, 0])
+          .domain([0, 1]);
     }
     render() {
         return <span></span>
