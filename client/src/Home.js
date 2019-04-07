0914088dea7c44bc90e07b252918232bfe8cfea8
KennethSee <kennethseede@gmail.com>
2019-04-06 21:58:25 -0400

    two branches now merged

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,31 +4,31 @@ import "./home.css";
 class Home extends Component {
   render() {
     return (
-      <div class="gallery">
-        <div class="card">
-          <div class="circle">
-            <i class="fas fa-4x fa-phone-volume"></i>
+      <div className="gallery">
+        <div className="card">
+          <div className="circle">
+            <i className="fas fa-4x fa-phone-volume"></i>
           </div>
           <div>
-            <p class="title">Meetings</p>
+            <p className="title">Meetings</p>
             <p>25 meetings</p>
           </div>
         </div>
-        <div class="card">
-          <div class="circle">
-            <i class="fas fa-4x fa-file"></i>
+        <div className="card">
+          <div className="circle">
+            <i className="fas fa-4x fa-file"></i>
           </div>
           <div>
-            <p class="title">Work</p>
+            <p className="title">Work</p>
             <p>41 Stories</p>
           </div>
         </div>
-        <div class="card">
-          <div class="circle">
-            <i class="fas fa-4x fa-utensils"></i>
+        <div className="card">
+          <div className="circle">
+            <i className="fas fa-4x fa-utensils"></i>
           </div>
           <div>
-            <p class="title">Break</p>
+            <p className="title">Break</p>
             <p>4 lunches</p>
           </div>
         </div>
