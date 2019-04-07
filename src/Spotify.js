a29672ce8ade9800e2b91a059a40b8ddb628889b
Crystal Rhee <crystal.rhee@gmail.com>
2019-04-06 20:30:32 -0400

    fixed table for top artists

--- src/Spotify.js
+++ src/Spotify.js
@@ -10,21 +10,45 @@ class Spotify extends Component {
             <h3>Top 5 Artists</h3>
           </div>
           <div></div>
-          <div class="table">
-            <thead>
+          <table>
+            <tr>
               <th colspan="2">Artist</th>
+              <th>Song</th>
               <th>Times Played</th>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="w-1">
-                  <img class="avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"/>
-                </td>
-                <td>Ariana Grande</td>
-                <td>21</td>
-              </tr>
-            </tbody>
-          </div>
+            </tr>
+            <tr>
+              <td class="w-1">
+                <img class="avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"/>
+              </td>
+              <td>Ariana Grande</td>
+              <td>7 Rings</td>
+              <td>51</td>
+            </tr>
+            <tr>
+              <td class="w-1">
+                <img class="avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"/>
+              </td>
+              <td>Ariana Grande</td>
+              <td>Break up with your girlfriend, I'm bored</td>
+              <td>21</td>
+            </tr>
+            <tr>
+              <td class="w-1">
+                <img class="avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"/>
+              </td>
+              <td>Ariana Grande</td>
+              <td>Thank u, next</td>
+              <td>19</td>
+            </tr>
+            <tr>
+              <td class="w-1">
+                <img class="avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"/>
+              </td>
+              <td>Ariana Grande</td>
+              <td>God is a Woman</td>
+              <td>17</td>
+            </tr>
+          </table>
         </div>
       </div>
     );
